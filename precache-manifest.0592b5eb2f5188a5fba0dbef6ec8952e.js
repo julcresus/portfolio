21a125a938e89e79ca378ede239cfe89be4578eb
julcresus <cresusjulien@gmail.com>
2019-02-20 02:38:57 +0000

    Updates

---
+++ precache-manifest.0592b5eb2f5188a5fba0dbef6ec8952e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "beebbfad3fd5757e041e",
+    "url": "/portfolio/static/css/main.c78d1e24.chunk.css"
+  },
+  {
+    "revision": "beebbfad3fd5757e041e",
+    "url": "/portfolio/static/js/main.beebbfad.chunk.js"
+  },
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "885a16b6577949f68693",
+    "url": "/portfolio/static/css/2.8fc34f55.chunk.css"
+  },
+  {
+    "revision": "885a16b6577949f68693",
+    "url": "/portfolio/static/js/2.885a16b6.chunk.js"
+  },
+  {
+    "revision": "5a2585dada70e2b4dee72db24833de13",
+    "url": "/portfolio/index.html"
+  }
+];
